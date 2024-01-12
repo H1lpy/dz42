9ff70e75f92da1ac71924d9fe8639897f0a024e2
H1lpy <hilpy00@mail.ru>
2024-01-12 23:29:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n: ");
+            double n = double.Parse(Console.ReadLine());
+            double p = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                double s = 0;
+                for(int j = 0; j <= n; j++)
+                {
+                    s += Pow(i, 2.0) - 2.0 * j;
+                }
+                p += s;
+            }
+            Console.WriteLine(p);
+        }
+    }
+}
